069f5dde22d3c60af0a3aac808d2276293254359
Bisharat Ali <engineer.bisharatali@gmail.com>
2024-05-26 22:00:09 +0500

    Add practice file for Webview

---
+++ WebView/app/src/main/java/com/example/webview/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.webview
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
+
+class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val webview = findViewById<WebView>(R.id.webview)
+
+        webviewsetup(webview)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun webviewsetup(webview: WebView) {
+
+        webview.webViewClient = WebViewClient()
+
+        webview.apply {
+            settings.javaScriptEnabled = true
+            settings.safeBrowsingEnabled = true
+            loadUrl("https://github.com/engineer-bisharat-ali")
+        }
+    }
+
+
+}
